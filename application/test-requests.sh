f6ad892b18982725faed733c24570a802b1b8418
Charles Crawford <crawford.charles.g@gmail.com>
2021-11-11 16:40:33 -0700

    port numbers

--- application/test-requests.sh
+++ application/test-requests.sh
@@ -1,9 +1,9 @@
 # sample request for the single prediction
-curl --location --request POST 'http://0.0.0.0:5001/sentiment/predict-one' \
+curl --location --request POST 'http://0.0.0.0:500/sentiment/predict-one' \
 --header 'Content-Type: application/json' \
 --data-raw '{"plain_text": "Some text that could be good or bad."}'
 
 # sample request for the batch predictions
-curl --location --request POST '0.0.0.0:5001/sentiment/predict-batch' \
+curl --location --request POST '0.0.0.0:5000/sentiment/predict-batch' \
 --header 'Content-Type: application/json' \
 --data-raw '{"text_list": ["Some text that could be good or bad.", "Some good text.", "Some bad text."]}'
